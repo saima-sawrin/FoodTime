b8a8bb2f859cf06ec0d940be22490fd21868a629
Saima <saimasawrin324@gmail.com>
2023-05-14 13:04:01 +0600

    router

---
+++ src/Layout/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../Component/Header/Header';
+import Hero from '../Component/Hero/Hero';
+import Explore from '../Component/Explore/Explore';
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Hero></Hero>
+            <Explore></Explore>
+        </div>
+    );
+};
+
+export default Main;
